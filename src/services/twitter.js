a680cf98e60a6a81962172e5c2c08dc2a1ead811
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-04-19 00:56:59 +0200

    User better twitter strategy

--- src/services/twitter.js
+++ src/services/twitter.js
@@ -1,7 +1,13 @@
 'use strict'
 
+const cheerio = require('cheerio')
+const got = require('got')
+
 module.exports = async username => {
-  return `https://twitter.com/${username}/profile_image?size=original`
+  const { body } = await got(`https://mobile.twitter.com/${username}`)
+  const $ = cheerio.load(body)
+  const el = $('.avatar img').attr('src')
+  return el.replace('_normal', '_400x400')
 }
 
 module.exports.supported = {
