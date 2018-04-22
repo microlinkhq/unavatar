5e18aa891b4c51cc7463dc6e255423742c537590
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-04-22 20:17:52 +0200

    Ensure GitHub url exists

--- src/services/github.js
+++ src/services/github.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const { avatarSize } = require('../constant')
+const got = require('got')
 
-module.exports = async username =>
-  `https://github.com/${username}.png?size=${avatarSize}`
+module.exports = async username => {
+  try {
+    await got.head(`https://github.com/${username}.png?size=${avatarSize}`)
+  } catch (err) {
+    return null
+  }
+}
 
 module.exports.supported = {
   email: false,
