21486082ffefdd3b0ea0096a0eb3bcf5c346a3ba
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-11 17:49:15 +0200

    fix: lint

--- src/providers/youtube.js
+++ src/providers/youtube.js
@@ -2,14 +2,14 @@
 
 const { isNil, chain, get } = require('lodash')
 const { stringify } = require('querystring')
+const { URL } = require('url')
 const pAny = require('p-any')
-const url = require('url')
 const got = require('got')
 
 const { YOUTUBE_API_KEY } = require('../constant')
 
 const getUrl = async (username, { slugProp }) => {
-  const parts = url.parse(username).pathname.split('/')
+  const parts = new URL(username).pathname.split('/')
   const slug = chain(parts)
     .filter(p => p !== '')
     .last()
@@ -21,13 +21,10 @@ const getUrl = async (username, { slugProp }) => {
     key: YOUTUBE_API_KEY
   })
 
-  const { body } = await got(
-    'https://content.googleapis.com/youtube/v3/channels',
-    {
-      json: true,
-      query
-    }
-  )
+  const { body } = await got('https://content.googleapis.com/youtube/v3/channels', {
+    json: true,
+    query
+  })
 
   const avatarUrl = get(body, 'items[0].snippet.thumbnails.medium.url')
   if (isNil(avatarUrl)) {
@@ -37,10 +34,7 @@ const getUrl = async (username, { slugProp }) => {
 }
 
 module.exports = async username => {
-  return pAny([
-    getUrl(username, { slugProp: 'forUsername' }),
-    getUrl(username, { slugProp: 'id' })
-  ])
+  return pAny([getUrl(username, { slugProp: 'forUsername' }), getUrl(username, { slugProp: 'id' })])
 }
 
 module.exports.supported = {
