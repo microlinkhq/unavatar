8b00bd0225bb319e7f381c003ed2ad4519461dab
David Refoua <David@Refoua.me>
2019-08-20 07:59:42 +0430

    refactor: switch to cheerio

--- src/providers/deviantart.js
+++ src/providers/deviantart.js
@@ -1,26 +1,21 @@
 'use strict'
 
-const { filter } = require('lodash')
-const { JSDOM } = require('jsdom')
+const cheerio = require('cheerio')
 const got = require('got')
 
 const REGEX_PROFILE_URL = /^https?:\/\/(?:www\.)?deviantart\.com\/([\w\-]+)$/
 
 module.exports = async username => {
   const { body } = await got(`https://www.deviantart.com/${username}`)
-  const { window } = new JSDOM(body, { runScripts: 'dangerously' })
-  const canonUsername = window.document
-    .querySelector('head link[rel=canonical]')
-    .getAttribute('href')
+  const $ = cheerio.load(body)
+  const canonUsername = $('head link[rel=canonical]')
+    .attr('href')
     .replace(REGEX_PROFILE_URL, '$1')
-  const els = filter(
-    window.document.querySelectorAll('img.avatar,a[data-hook=user_link][data-icon]'),
-    el => {
-      const thisUsername = el.getAttribute('data-username') || el.getAttribute('title')
-      return canonUsername.toLowerCase() === thisUsername.toLowerCase()
-    }
-  )
-  return els[0].getAttribute('data-icon') || els[0].getAttribute('src')
+  const els = $('img.avatar,a[data-hook=user_link][data-icon]').filter((i, el) => {
+    const thisUsername = $(el).attr('data-username') || $(el).attr('title')
+    return canonUsername.toLowerCase() === thisUsername.toLowerCase()
+  })
+  return els.attr('data-icon') || els.attr('src')
 }
 
 module.exports.supported = {
