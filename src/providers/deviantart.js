74e972cb9be54d159bde22ecb2731d2c6e3033e0
David Refoua <David@Refoua.me>
2019-08-20 08:01:02 +0430

    fix: username regex

--- src/providers/deviantart.js
+++ src/providers/deviantart.js
@@ -3,7 +3,7 @@
 const cheerio = require('cheerio')
 const got = require('got')
 
-const REGEX_PROFILE_URL = /^https?:\/\/(?:www\.)?deviantart\.com\/([\w\-]+)$/
+const REGEX_PROFILE_URL = /^https?:\/\/(?:www\.)?deviantart\.com\/([\w\-]+)(?:\/.+)?$/
 
 module.exports = async username => {
   const { body } = await got(`https://www.deviantart.com/${username}`)
@@ -12,7 +12,7 @@ module.exports = async username => {
     .attr('href')
     .replace(REGEX_PROFILE_URL, '$1')
   const els = $('img.avatar,a[data-hook=user_link][data-icon]').filter((i, el) => {
-    const thisUsername = $(el).attr('data-username') || $(el).attr('title')
+    const thisUsername = $(el).attr('data-username') || $(el).attr('title') || ''
     return canonUsername.toLowerCase() === thisUsername.toLowerCase()
   })
   return els.attr('data-icon') || els.attr('src')
