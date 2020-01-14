7626719f196541ad4ef2242eff81c3201fd60f45
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-01-14 13:18:34 +0100

    build: handle error

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const server = require('..')
 
 const port = process.env.PORT || process.env.port || 3000
 
+server.on('error', err => {
+  debug({ status: 'error', message: err.message, trace: err.stack })
+  process.exit(1)
+})
+
 server.listen(port, function () {
   debug({
     status: 'listening',
