167037d4c8d4b7f627466fb48b9a0b1a5df82f9b
Zadkiel <hello@zadkiel.fr>
2020-12-07 22:18:38 +0100

    fix: instagram image fetch (#118)

--- src/providers/instagram.js
+++ src/providers/instagram.js
@@ -5,13 +5,12 @@ const { JSDOM } = require('jsdom')
 const got = require('got')
 
 const AVATAR_URL = {
-  big: '_sharedData.entry_data.ProfilePage[0].graphql.user.profile_pic_url_hd',
-  normal: '_sharedData.entry_data.ProfilePage[0].graphql.user.profile_pic_url'
+  big: 'graphql.user.profile_pic_url_hd',
+  normal: 'graphql.user.profile_pic_url'
 }
 
 module.exports = async username => {
-  const { body } = await got(`https://www.instagram.com/${username}?__a=1`)
-  const { window } = new JSDOM(body, { runScripts: 'dangerously' })
+  const body = await got(`https://www.instagram.com/${username}/?__a=1`).json()
   const avatarUrl = get(window, AVATAR_URL.big) || get(window, AVATAR_URL.normal)
   return avatarUrl ? new URL(avatarUrl).toString() : null
 }
