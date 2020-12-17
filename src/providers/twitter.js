74fd5f2ba449dfd7e7c2d446eaa4dfd3b860c942
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-12-17 18:18:40 +0100

    chore: add console.logs

--- src/providers/twitter.js
+++ src/providers/twitter.js
@@ -10,16 +10,20 @@ const getAvatarUrl = url => url.replace(REGEX_IMG_MODIFIERS, `${ORIGINAL_IMG_SIZ
 
 module.exports = async username => {
   // Get a fresh guest token
+  console.log('Requesting Twitter user', username)
   const { body: guestBody } = await got('https://twitter.com', {
     headers: {
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
     }
   })
+  console.log('Got twitter.com response', guestBody)
   const guestToken = guestBody.match(/gt=[0-9]*/gi)[0].slice(3)
+  console.log('Extracted guest token', guestToken)
 
   // Request api endpoint with guest token. Bearer auth is hardcoded to this value.
   const payload = { screen_name: username, withHighlightedLabel: true }
+  console.log('Api request payload', payload)
   const { body: apiBody } = await got(
     `https://twitter.com/i/api/graphql/ZRnOhhXPwue_JGILb9TNug/UserByScreenName?variables=${encodeURIComponent(
       JSON.stringify(payload)
@@ -32,7 +36,9 @@ module.exports = async username => {
       }
     }
   )
+  console.log('Got api response', apiBody)
   const imgURL = JSON.parse(apiBody).data.user.legacy.profile_image_url_https
+  console.log('Extracted img url', imgURL)
 
   // Use _400x400 image size
   return getAvatarUrl(imgURL)
