b0efd2899ac3a652ee614dc46120051ee8976cb5
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-12-17 10:03:26 +0100

    chore(feature): add Substack provider

---
+++ src/providers/substack.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const cheerio = require('cheerio')
+const got = require('got')
+
+module.exports = async username => {
+  const { body } = await got(`https://${username}.substack.com`)
+  const $ = cheerio.load(body)
+  return $('img.publication-logo').attr('src')
+}
+
+module.exports.supported = {
+  email: false,
+  username: true,
+  domain: false
+}
