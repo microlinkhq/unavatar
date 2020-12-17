9668275acfb8f643398008c6e3d7afa54975264e
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-12-17 19:07:23 +0100

    fix:  use different method to get a guest token

--- src/providers/twitter.js
+++ src/providers/twitter.js
@@ -5,24 +5,28 @@ const got = require('got')
 // https://dev.twitter.com/basics/user-profile-images-and-banners
 const REGEX_IMG_MODIFIERS = /_(?:bigger|mini|normal)\./
 const ORIGINAL_IMG_SIZE = '_400x400'
+const TWITTER_BEARER_TOKEN =
+  'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
 
 const getAvatarUrl = url => url.replace(REGEX_IMG_MODIFIERS, `${ORIGINAL_IMG_SIZE}.`)
 
 module.exports = async username => {
   // Get a fresh guest token
-  console.log('Requesting Twitter user', username)
-  const { body: guestBody } = await got(
-    `https://twitter.com/?prefetchTimestamp=${Date.now().toString()}`,
-    {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-      }
+  const { body: guestBody } = await got.post('https://api.twitter.com/1.1/guest/activate.json', {
+    https: {
+      rejectUnauthorized: false
+    },
+    responseType: 'json',
+    headers: {
+      authorization: TWITTER_BEARER_TOKEN,
+      origin: 'https://twitter.com',
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
     }
-  )
-  console.log('Got twitter.com response', guestBody)
-  const guestToken = guestBody.match(/gt=[0-9]*/gi)[0].slice(3)
-  console.log('Extracted guest token', guestToken)
+  })
+
+  const guestToken = guestBody.guest_token
+  console.log(guestToken)
 
   // Request api endpoint with guest token. Bearer auth is hardcoded to this value.
   const payload = { screen_name: username, withHighlightedLabel: true }
@@ -33,13 +37,12 @@ module.exports = async username => {
     )}`,
     {
       headers: {
-        authorization:
-          'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+        authorization: TWITTER_BEARER_TOKEN,
         'x-guest-token': guestToken
       }
     }
   )
-  console.log('Got api response', apiBody)
+  // console.log('Got api response', apiBody)
   const imgURL = JSON.parse(apiBody).data.user.legacy.profile_image_url_https
   console.log('Extracted img url', imgURL)
 
