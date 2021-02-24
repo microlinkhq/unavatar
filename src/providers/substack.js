27b4f8028d01b9ddc2e1d414c0f8d5108752a58a
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-02-24 10:00:29 +0100

    build: increment substack image size

--- src/providers/substack.js
+++ src/providers/substack.js
@@ -6,7 +6,7 @@ const got = require('got')
 module.exports = async username => {
   const { body } = await got(`https://${username}.substack.com`)
   const $ = cheerio.load(body)
-  return $('link[rel=apple-touch-icon][sizes=120x120]').attr('href')
+  return $('link[rel=apple-touch-icon][sizes=180x180]').attr('href')
 }
 
 module.exports.supported = {
