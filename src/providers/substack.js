a08e5c1ef598c8d46be06810874aef55fe338a2c
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-07-12 12:00:10 +0200

    chore(substack): improve image resolution

--- src/providers/substack.js
+++ src/providers/substack.js
@@ -6,7 +6,7 @@ const got = require('got')
 module.exports = async username => {
   const { body } = await got(`https://${username}.substack.com`)
   const $ = cheerio.load(body)
-  return $('link[rel=apple-touch-icon][sizes=180x180]').attr('href')
+  return $('.publication-logo').attr('src')
 }
 
 module.exports.supported = {
