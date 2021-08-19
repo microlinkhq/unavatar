ae80bfd1660072160f6502e3bd4321b5f1ca9fc2
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-08-19 14:22:10 +0200

    build: update dependencies

--- src/send/cache.js
+++ src/send/cache.js
@@ -13,8 +13,6 @@ module.exports =
     : require('cacheable-response')({
       ttl: CACHE_TTL,
       staleTtl: 0,
-      get: async ({ req, res, fn }) => ({
-        data: await fn(req, res)
-      }),
+      get: async ({ req, res, fn }) => ({ data: await fn(req, res) }),
       send: ({ req, res, data }) => send({ req, res, ...data })
     })
