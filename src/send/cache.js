bb5d1ab5b1cb8cc7e7ae4b421d0299dc0040836f
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-08-27 22:45:00 +0200

    build: remove mongo logic

--- src/send/cache.js
+++ src/send/cache.js
@@ -1,18 +1,10 @@
 'use strict'
 
-const KeyvMongo = require('@keyvhq/mongo')
-const Keyv = require('@keyvhq/core')
-
-const { CACHE_TTL, MONGO_URI } = require('../constant')
+const { CACHE_TTL } = require('../constant')
 
 const send = require('.')
 
-const store = MONGO_URI ? new KeyvMongo(MONGO_URI) : new Map()
-
-const cache = new Keyv({ namespace: null, store })
-
 module.exports = require('cacheable-response')({
-  cache: cache,
   ttl: CACHE_TTL,
   staleTtl: 0,
   get: async ({ req, res, fn }) => ({ data: await fn(req, res) }),
