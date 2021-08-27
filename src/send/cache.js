d921205e8a42bcb79cc6d51046205994541b19fa
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-08-27 13:27:42 +0200

    chore: add mongo cache support

--- src/send/cache.js
+++ src/send/cache.js
@@ -1,18 +1,20 @@
 'use strict'
 
-const { CACHE_TTL, NODE_ENV } = require('../constant')
+const KeyvMongo = require('@keyvhq/mongo')
+const Keyv = require('@keyvhq/core')
+
+const { CACHE_TTL, MONGO_URI } = require('../constant')
 
 const send = require('.')
 
-module.exports =
-  NODE_ENV === 'development'
-    ? async ({ req, res, fn }) => {
-        const data = await fn(req, res)
-        return send({ req, res, ...data })
-      }
-    : require('cacheable-response')({
-      ttl: CACHE_TTL,
-      staleTtl: 0,
-      get: async ({ req, res, fn }) => ({ data: await fn(req, res) }),
-      send: ({ req, res, data }) => send({ req, res, ...data })
-    })
+const store = MONGO_URI ? new KeyvMongo() : new Map()
+
+const cache = new Keyv({ namespace: null, store })
+
+module.exports = require('cacheable-response')({
+  cache: cache,
+  ttl: CACHE_TTL,
+  staleTtl: 0,
+  get: async ({ req, res, fn }) => ({ data: await fn(req, res) }),
+  send: ({ req, res, data }) => send({ req, res, ...data })
+})
