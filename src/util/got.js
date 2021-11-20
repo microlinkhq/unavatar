525ed95a619a09dd24a3ce6296a6b47f30dfcd1e
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-11-20 22:58:46 +0100

    chore: setup user agent and tls

--- src/util/got.js
+++ src/util/got.js
@@ -1,15 +1,30 @@
 'use strict'
 
+const uniqueRandomArray = require('unique-random-array')
 const CacheableLookup = require('cacheable-lookup')
+const userAgents = require('top-user-agents')
+const tlsHook = require('https-tls/hook')
 const got = require('got')
+
+const randomUserAgent = uniqueRandomArray(userAgents)
 
 const dnsCache = new CacheableLookup()
 dnsCache.servers = ['1.1.1.1', '1.0.0.1']
 
+const userAgentHook = options => {
+  options.headers['user-agent'] = randomUserAgent()
+}
+
 const gotOpts = {
   dnsCache,
   https: { rejectUnauthorized: false },
-  retry: 0
+  retry: 0,
+  headers: {
+    'user-agent': undefined
+  },
+  hooks: {
+    beforeRequest: [userAgentHook, tlsHook]
+  }
 }
 
 module.exports = got.extend(gotOpts)
