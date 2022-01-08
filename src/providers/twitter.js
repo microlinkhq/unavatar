d2cc0e242cfbd5a5984a8ef4e1dfefe91850e5da
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-01-08 18:37:59 +0100

    fix: ensure to wait until twitter dom is loaded

--- src/providers/twitter.js
+++ src/providers/twitter.js
@@ -7,7 +7,11 @@ const REGEX_IMG_MODIFIERS = /_(?:bigger|mini|normal)\./
 const ORIGINAL_IMG_SIZE = '_400x400'
 
 module.exports = async (username, { headers = {} } = {}) => {
-  const { data } = await mql(`https://twitter.com/${username}`, { apiKey: headers['x-api-key'] })
+  const { data } = await mql(`https://twitter.com/${username}`, {
+    waitForTimeout: 700,
+    apiKey: headers['x-api-key'],
+    force: true
+  })
   const avatarUrl = get(data, 'image.url', '')
   return avatarUrl.replace(REGEX_IMG_MODIFIERS, `${ORIGINAL_IMG_SIZE}.`)
 }
