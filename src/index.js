99ae3991d2560611fa1d75d13144dc86bd8f9fbf
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-01-10 22:34:52 +0100

    feat: add rate limiter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 'use strict'
 
+const RateLimiter = require('async-ratelimiter')
+const { getClientIp } = require('request-ip')
 const { forEach } = require('lodash')
 const express = require('express')
+const Redis = require('ioredis')
 const path = require('path')
-const { Router } = express
 
+const { REDIS_URI, LOG_LEVEL } = require('./constant')
 const { providers } = require('./providers')
-const { LOG_LEVEL } = require('./constant')
 
 const ssrCache = require('./send/cache')
 const avatar = require('./avatar')
+const { Router } = express
+
+const rateLimiter = new RateLimiter({
+  db: new Redis(REDIS_URI),
+  duration: 86400000,
+  max: 1000,
+  namespace: 'unavatar'
+})
 
 const router = Router()
 
@@ -18,6 +28,20 @@ router.use(
     crossOriginResourcePolicy: false
   })
 )
+
+router.use(async (req, res, next) => {
+  const clientIp = getClientIp(req)
+  const limit = await rateLimiter.get({ id: clientIp })
+
+  if (!res.finished && !res.headersSent) {
+    res.setHeader('X-Rate-Limit-Limit', limit.total)
+    res.setHeader('X-Rate-Limit-Remaining', Math.max(0, limit.remaining - 1))
+    res.setHeader('X-Rate-Limit-Reset', limit.reset)
+  }
+
+  return limit.remaining ? next() : res.status(429).end('Daily quota reached')
+})
+
 router.use(require('compression')())
 router.use(require('cors')())
 router.use(require('morgan')(LOG_LEVEL))
