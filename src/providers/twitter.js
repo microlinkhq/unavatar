ada83efe52b1069b8437a344261323c2537b1136
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-01-02 02:25:04 +0100

    refactor(twitter): remove normalization (#210)
    * refactor(twitter): remove normalization
    
    Since Microlink API is using [metascraper-twitter](https://github.com/microlinkhq/metascraper/tree/master/packages/metascraper-twitter), this code is no more needed
    
    * build: tweaks

--- src/providers/twitter.js
+++ src/providers/twitter.js
@@ -3,17 +3,14 @@
 const mql = require('@microlink/mql')
 const { get } = require('lodash')
 
-const REGEX_IMG_MODIFIERS = /_(?:bigger|mini|normal)\./
-const ORIGINAL_IMG_SIZE = '_400x400'
-
 module.exports = async (username, { headers = {} } = {}) => {
   const { data } = await mql(`https://twitter.com/${username}`, {
     waitForTimeout: 700,
     apiKey: headers['x-api-key'],
     force: true
   })
-  const avatarUrl = get(data, 'image.url', '')
-  return avatarUrl.replace(REGEX_IMG_MODIFIERS, `${ORIGINAL_IMG_SIZE}.`)
+
+  return get(data, 'image.url', '')
 }
 
 module.exports.supported = {
