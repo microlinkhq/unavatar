de8d8bac81371ce14156c96b4fd6a51137268182
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-25 20:02:53 +0100

    refactor: update youtube selector

--- src/providers/youtube.js
+++ src/providers/youtube.js
@@ -1,41 +1,12 @@
 'use strict'
 
-const { get } = require('lodash')
-const pAny = require('p-any')
-
-const Error = require('../util/error')
+const cheerio = require('cheerio')
 const got = require('../util/got')
 
-const { YOUTUBE_API_KEY } = require('../constant')
-
-const getAvatarUrl = async (username, bySlugProp) => {
-  const { body } = await got(
-    'https://content.googleapis.com/youtube/v3/channels',
-    {
-      searchParams: {
-        part: 'id,snippet',
-        [bySlugProp]: username,
-        key: YOUTUBE_API_KEY
-      },
-      responseType: 'json'
-    }
-  )
-
-  const value = get(body, 'items[0].snippet.thumbnails.medium.url')
-  if (!value) {
-    throw new Error({
-      statusCode: 404,
-      message: `Avatar for \`${bySlugProp}\` as \`${username}\` not found`
-    })
-  }
-  return value
-}
-
-module.exports = function youtube (username) {
-  return pAny([
-    getAvatarUrl(username, 'forUsername'),
-    getAvatarUrl(username, 'id')
-  ])
+module.exports = async function gitlab (username) {
+  const { body: html } = await got(`https://www.youtube.com/${username}`)
+  const $ = cheerio.load(html)
+  return $('meta[property="og:image"]').attr('content')
 }
 
 module.exports.supported = {
