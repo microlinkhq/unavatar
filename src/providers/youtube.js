f24d5d174c9630de14136c734737fb69fcfdcc1e
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-25 20:19:19 +0100

    fix: prefix youtube handler

--- src/providers/youtube.js
+++ src/providers/youtube.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 const got = require('../util/got')
 
 module.exports = async function gitlab (username) {
-  const { body: html } = await got(`https://www.youtube.com/${username}`)
+  const { body: html } = await got(`https://www.youtube.com/@${username}`)
   const $ = cheerio.load(html)
   return $('meta[property="og:image"]').attr('content')
 }
