20fd9e80ecb6a16e8cb1720b64aee33ed11bb42c
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-08-06 12:03:39 +0200

    chore(twitter): use crawler user agent (#273)

--- src/util/got.js
+++ src/util/got.js
@@ -1,18 +1,17 @@
 'use strict'
 
 const uniqueRandomArray = require('unique-random-array')
-const userAgents = require('top-user-agents')
 const tlsHook = require('https-tls/hook')
 const got = require('got')
 
-const randUserAgent = uniqueRandomArray(userAgents)
+const randomUserAgent = uniqueRandomArray(require('top-user-agents'))
 
 const userAgentHook = options => {
   if (
     options.headers['user-agent'] ===
     'got (https://github.com/sindresorhus/got)'
   ) {
-    options.headers['user-agent'] = randUserAgent()
+    options.headers['user-agent'] = randomUserAgent()
   }
 }
 
