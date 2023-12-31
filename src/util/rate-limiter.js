3fd17eca9f60da372595da037302fc87ec93cd1d
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-12-31 10:07:06 +0100

    refactor: better redis utility (#299)

--- src/util/rate-limiter.js
+++ src/util/rate-limiter.js
@@ -4,7 +4,7 @@ const RateLimiter = require('async-ratelimiter')
 
 const { RATE_LIMIT_WINDOW, RATE_LIMIT } = require('../constant')
 
-const db = require('./redis').cache
+const db = require('./redis')
 
 module.exports = db
   ? new RateLimiter({
