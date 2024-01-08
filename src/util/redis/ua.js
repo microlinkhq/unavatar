fb18bbb7aa23d6c19c7fb09b5c783e9acda51a05
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-01-08 09:37:27 +0100

    refactor: unify redis connection into REDIS_URI

--- src/util/redis/ua.js
+++ src/util/redis/ua.js
@@ -1,5 +1,5 @@
 'use strict'
 
-const { REDIS_UA_URI } = require('../../constant')
+const { REDIS_URI } = require('../../constant')
 
-module.exports = require('./create')(REDIS_UA_URI)
+module.exports = require('./create')(REDIS_URI)
