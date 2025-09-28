07e726815d7ef86f7f9ee249e0e455142af14cc5
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-09-28 22:20:52 +0200

    chore: better browserless setup

--- src/util/browserless.js
+++ src/util/browserless.js
@@ -17,7 +17,14 @@ const getArgs = () => {
     '--disk-cache-size=33554432', // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
     '--enable-features=SharedArrayBuffer', // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
     `--disk-cache-dir=${CACHE_DIR}`,
-    `--user-data-dir=${DATA_DIR}`
+    `--user-data-dir=${DATA_DIR}`,
+    '--disable-font-subpixel-positioning', // https://github.com/puppeteer/puppeteer/issues/2410#issuecomment-2886054614
+    '--no-startup-window',
+    '--disable-gpu',
+    '--use-gl=angle',
+    '--use-angle=swiftshader',
+    '--enable-unsafe-swiftshader',
+    '--renderer-process-limit=2'
   ])
 
   return { PUPPETEER_DIR, DATA_DIR, CACHE_DIR, args }
@@ -25,7 +32,10 @@ const getArgs = () => {
 
 const browser = createBrowser({
   args: getArgs().args,
-  puppeteer
+  puppeteer,
+  waitForInitialPage: false,
+  pipe: true,
+  dumpio: true
 })
 
 module.exports = () => browser
