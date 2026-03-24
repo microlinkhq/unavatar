f8522041e7d2438d7e5b920c50ef3797cb5bb932
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-24 23:20:55 +0100

    chore(patreon): support campaign markup

--- src/providers/patreon.js
+++ src/providers/patreon.js
@@ -2,9 +2,37 @@
 
 const { $jsonld } = require('@metascraper/helpers')
 
-module.exports = ({ createHtmlProvider }) =>
+const unescapeUnicode = str =>
+  str.replace(/\\u([0-9a-fA-F]{4})/g, (_, hex) =>
+    String.fromCharCode(parseInt(hex, 16))
+  )
+
+const getRscAvatar = $ => {
+  let url
+  $('script').each((_, el) => {
+    const text = $(el).html() || ''
+    if (!text.includes('avatarPhotoImageUrls')) return
+    const match = text.match(
+      /avatarPhotoImageUrls[\s\S]*?\\"original\\":\\"((?:[^\\"]|\\u[0-9a-fA-F]{4})+)/
+    )
+    if (match) {
+      url = unescapeUnicode(match[1])
+      return false
+    }
+  })
+  return url
+}
+
+const getAvatar = $ =>
+  $jsonld('mainEntity.image.contentUrl')($) || getRscAvatar($)
+
+const factory = ({ createHtmlProvider }) =>
   createHtmlProvider({
     name: 'patreon',
     url: username => `https://www.patreon.com/${username}`,
-    getter: $ => $jsonld('mainEntity.image.thumbnailUrl')($)
+    getter: getAvatar
   })
+
+factory.getAvatar = getAvatar
+
+module.exports = factory
