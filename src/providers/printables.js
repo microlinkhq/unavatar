f8734ce452d4bda4d9d98370abbeca5c67707924
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-25 10:12:57 +0100

    fix: remove unnecessary wrapping

--- src/providers/printables.js
+++ src/providers/printables.js
@@ -5,7 +5,10 @@ const randomCrawlerAgent = require('../util/crawler-agent')
 module.exports = ({ createHtmlProvider, getOgImage }) =>
   createHtmlProvider({
     name: 'printables',
-    url: input => `https://www.printables.com/${input.startsWith('@') ? input : `@${input}`}`,
-    getter: $ => getOgImage($),
+    url: input =>
+      `https://www.printables.com/${
+        input.startsWith('@') ? input : `@${input}`
+      }`,
+    getter: getOgImage,
     htmlOpts: () => ({ headers: { 'user-agent': randomCrawlerAgent() } })
   })
