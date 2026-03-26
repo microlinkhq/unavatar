c4cd4af384dc570cea1dea284effd47dd5ca9ad8
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-26 23:04:02 +0100

    chore: use well known crawlers

--- src/util/crawler-agent.js
+++ src/util/crawler-agent.js
@@ -2,4 +2,6 @@
 
 const uniqueRandomArray = require('unique-random-array')
 
-module.exports = uniqueRandomArray(require('top-crawler-agents'))
+module.exports = uniqueRandomArray(
+  require('top-crawler-agents').filter(agent => agent.startsWith('Slackbot'))
+)
