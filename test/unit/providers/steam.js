672b99175f8998108078870213e713126bbb802d
Jose Francisco 'Kiko' Verdú Gambín <josefrancisco.verdu@gmail.com>
2026-04-11 23:22:11 +0300

    fix(steam): private profiles (#547)

--- test/unit/providers/steam.js
+++ test/unit/providers/steam.js
@@ -7,6 +7,12 @@ const getOgImage = require('../../../src/util/get-og-image')
 
 const { getAvatarUrl } = require('../../../src/providers/steam')
 
+const createSteam = () =>
+  require('../../../src/providers/steam')({
+    createHtmlProvider: opts => opts,
+    getOgImage
+  })
+
 test('.getAvatarUrl defaults to id path', t => {
   t.is(getAvatarUrl('Kikobeats'), 'https://steamcommunity.com/id/Kikobeats')
 })
@@ -39,13 +45,7 @@ test('.getter extracts og:image from steam html markup', t => {
     </html>
   `
   const $ = cheerio.load(html)
-
-  const createHtmlProvider = opts => opts
-
-  const steam = require('../../../src/providers/steam')({
-    createHtmlProvider,
-    getOgImage
-  })
+  const steam = createSteam()
 
   t.is(
     steam.getter($),
@@ -62,16 +62,35 @@ test('.getter extracts og:image from steam group html markup', t => {
     </html>
   `
   const $ = cheerio.load(html)
-
-  const createHtmlProvider = opts => opts
-
-  const steam = require('../../../src/providers/steam')({
-    createHtmlProvider,
-    getOgImage
-  })
+  const steam = createSteam()
 
   t.is(
     steam.getter($),
     'https://avatars.fastly.steamstatic.com/34c730c9c7dac891ffa089ab7e0b7ed333235700_full.jpg'
+  )
+})
+
+test('.getter falls back to profile avatar when og:image is steam default image', t => {
+  const html = `
+    <html>
+      <head>
+        <meta property="og:image" content="https://community.fastly.steamstatic.com/public/shared/images/responsive/steam_share_image.jpg" />
+      </head>
+      <body>
+        <div class="playerAvatar profile_header_size offline">
+          <picture>
+            <source srcset="https://avatars.fastly.steamstatic.com/c5d56249ee5d28a07db4ac9f7f60af961fab5426_full.jpg" />
+            <img srcset="https://avatars.fastly.steamstatic.com/c5d56249ee5d28a07db4ac9f7f60af961fab5426_full.jpg" />
+          </picture>
+        </div>
+      </body>
+    </html>
+  `
+  const $ = cheerio.load(html)
+  const steam = createSteam()
+
+  t.is(
+    steam.getter($),
+    'https://avatars.fastly.steamstatic.com/c5d56249ee5d28a07db4ac9f7f60af961fab5426_full.jpg'
   )
 })
